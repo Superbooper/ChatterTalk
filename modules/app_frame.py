207f8791dac35b28e58bc1abd01057b6c19bef06
Nathan <130393001+Superbooper@users.noreply.github.com>
2025-12-08 12:06:04 -0500

    new file:   .vscode/settings.json 	new file:   modules/__pycache__/app_frame.cpython-313.pyc 	modified:   modules/app_frame.py 	modified:   modules/usr-speak.py 	new file:   testing/mic_info_test.py

--- modules/app_frame.py
+++ modules/app_frame.py
@@ -5,10 +5,10 @@ class chatterTalk(tk.Tk):
         self.title("Chatter Talk")
         self.geometry('500x500')
         self.create_widgets()
-        self.create_buttons()
+        #self.create_buttons()
     def create_widgets(self):
         self.label = tk.Label(self, text='Welcome to Chatter Talk')
         self.label.pack(pady=10)
-    def create_buttons(self):
-        self.input
-        self.clarify
+    #def create_buttons(self):
+        #self.input
+        #self.clarify
--- modules/usr-speak.py
+++ modules/usr-speak.py
@@ -1,12 +1,12 @@
-import speech_recognition
-import os
-import pyaudio
+import speech_recognition as sr
+import pyttsx3
 import sphinx
 # get mic audio
 def user_speech_grab():
-    r=speech_recognition.Recognizer()
-    with speech_recognition.Microphone() as source:
+    r= sr.Recognizer()
+    with sr.Microphone(device_index=16) as source:
         #space for tk console slot here
         audio = r.listen(source)
-    #recognize speech
-    r.recognize_wi
+        text = r.recognize_sphinx(audio)
+        print(text)
+user_speech_grab()
